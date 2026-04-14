640a8c6f3259d377f33064e744519790599e8fe9
HARSH KUMAR <xt.harshh@gmail.com>
2026-04-14 23:40:09 +0530

    first commit

--- apps/marketplace/auth_serializers.py
+++ apps/marketplace/auth_serializers.py
@@ -10,17 +10,23 @@ class RegisterSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=150)
     password = serializers.CharField(write_only=True, min_length=8)
     email = serializers.EmailField(required=False, allow_blank=True)
+    phone_number = serializers.CharField(max_length=20, required=False, allow_blank=True)
     role = serializers.ChoiceField(choices=UserProfile.ROLE_CHOICES, default='customer')
 
     def create(self, validated_data):
         role = validated_data.pop('role', 'customer')
+        phone_number = validated_data.pop('phone_number', '')
         password = validated_data.pop('password')
         user = User.objects.create_user(**validated_data)
         user.set_password(password)
         user.save()
         profile, _ = UserProfile.objects.get_or_create(user=user)
         profile.role = role
-        profile.save(update_fields=['role'])
+        if phone_number:
+            profile.phone_number = phone_number
+            profile.save(update_fields=['role', 'phone_number'])
+        else:
+            profile.save(update_fields=['role'])
         return user
 
 
